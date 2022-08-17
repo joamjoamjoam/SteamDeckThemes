af9d617d64334c476cc450bf886ce918a431680b
joamjoamjoam <trentcalllan@yahoo.com>
2022-08-16 18:37:26 -0700

    Use  jsonTemplate to support Extended FIle Support

--- Wallpapers Theme/Wallpapers/updateImages.py
+++ Wallpapers Theme/Wallpapers/updateImages.py
@@ -1,36 +1,16 @@
-import os, shutil, json, base64, time
+import os, shutil, json, base64, time, re
 
 validExtensions = [".jpg", ".png", ".svg", ".gif", ".jpeg"]
 
 cssDir = "generatedCSSFiles"
-lockScreenCssDir = "lockscreen"
-homeScreenCssDir = "homescreen"
-libraryCssDir = "library"
-keyboardCssDir = "keyboard"
 b64Dir = "sharedB64Images"
-lockScreenVarName = "--WPRLockScreenWallPaper"
-homeScreenVarName = "--WPRHomeScreenWallPaper"
-libraryVarName = "--WPRLibraryWallPaper"
-keyboardVarName = "--WPRKeyboardWallPaper"
-
-# Main CSS File paths
-lockScreenMainPath = "wallpaperCSS/lockScreen.css"
-homeScreenMainPath = "wallpaperCSS/homeScreen.css"
-libraryMainPath = "wallpaperCSS/library.css"
-keyboardMainPath = "wallpaperCSS/keyboard.css"
+cssVariableTemplate = f":root{{\n\t--<customVarName>: var(--<imageVariableName>);\n}}"
 
 cssFileTypes = { 
-    b64Dir: f":root{{\n\t<variableName>: url('data:image/<imgType>;base64,<b64String>') !important;\n}}", \
-    lockScreenCssDir: f":root{{\n\t{lockScreenVarName}: var(<variableName>);\n}}", \
-    homeScreenCssDir: f":root{{\n\t{homeScreenVarName}: var(<variableName>);\n}}", \
-    libraryCssDir: f":root{{\n\t{libraryVarName}: var(<variableName>);\n}}", \
-    keyboardCssDir: f":root{{\n\t{keyboardVarName}: var(<variableName>);\n}}" \
+    b64Dir: f":root{{\n\t--<imageVariableName>: url('data:image/<imgType>;base64,<b64String>') !important;\n}}"
 }
 
 exitTimeout = 5
-
-
-themeJsonBase = "{ \"name\": \"Wallpapers\", \"version\": \"v1.0\", \"author\": \"joamjoamjoam\", \"target\": \"System-Wide\", \"description\": \"Sets Wallpapers. What did you Expect?\", \"manifest_version\": 2, \"inject\": { }, \"patches\": {  \"Disable Home Overlay\": { \"default\": \"Yes\", \"type\": \"dropdown\", \"values\" : { \"No\" : {}, \"Yes\" : { \"homeScreenOverlay/disabled.css\" : [ \"SP\" ] } } }, \"Home Screen Image\": { \"default\": \"None\", \"type\": \"dropdown\", \"values\":{ \"None\": {} } }, \"Lock Screen Image\": { \"default\": \"None\", \"type\": \"dropdown\", \"values\":{ \"None\": {} } }, \"Library Background Image\": { \"default\": \"None\", \"type\": \"dropdown\", \"values\":{ \"None\": {} } }, \"Keyboard Background Image\": { \"default\": \"None\", \"type\": \"dropdown\", \"values\":{ \"None\": {} } } } }"
 
 def getB64ForFile(file):
     rv = ""
@@ -76,18 +56,18 @@ def writeCSSType(type, filePath, varName):
         
         fileName = f"{varName}.css"
         
-        variableName = "--WPRImage" + varName.replace(" ", "")
+        variableName = "WPRImage" + varName.replace(" ", "")
 
         if type == "sharedB64Images":
             b64 = getB64ForFile(filePath)
 
             f = open(f"{cssDir}/{type}/{fileName}" , "w")
-            tmpStr = cssFileTypes[type].replace("<variableName>", variableName).replace("<b64String>", b64).replace("<imgType>", getImgTypeTagForFileExt(fileInfo[1]))
+            tmpStr = cssFileTypes[type].replace("<imageVariableName>", variableName).replace("<b64String>", b64).replace("<imgType>", getImgTypeTagForFileExt(fileInfo[1]))
             f.write(tmpStr)
             f.close()
         else:
             f = open(f"{cssDir}/{type}/{fileName}" , "w")
-            f.write(cssFileTypes[type].replace("<variableName>", variableName))
+            f.write(cssFileTypes[type].replace("<imageVariableName>", variableName))
             f.close()
     except Exception as e:
         print(f"Error  writing type {type}: {str(e)}")
@@ -102,15 +82,26 @@ def main():
     if os.path.isdir(cssDir):
         shutil.rmtree(cssDir)
     
+    jsonTemplate = open("themeTemplate.json")
+    themeJson = json.load(jsonTemplate)
+    jsonTemplate.close()
     # Create Folder Structure
     os.mkdir(cssDir)
-    os.mkdir(cssDir + "/" + lockScreenCssDir)
-    os.mkdir(cssDir + "/" + homeScreenCssDir)
-    os.mkdir(cssDir + "/" + libraryCssDir)
-    os.mkdir(cssDir + "/" + b64Dir)
-    os.mkdir(cssDir + "/" + keyboardCssDir)
+    os.mkdir(f"{cssDir}/{b64Dir}")
 
-    themeJson = json.loads(themeJsonBase)
+    for k,v in themeJson["patches"].items():
+        if v["type"] == "dropdown-image":
+            if "var" in v.keys() and re.match(r"[A-Za-z0-9_-]+", v["var"]):
+                tmpVar = v["var"]
+                print(tmpVar)
+                os.mkdir(f'{cssDir}/{tmpVar}')
+                cssFileTypes[v["var"]] = cssVariableTemplate.replace("<customVarName>", v["var"])
+                print(cssFileTypes)
+            else:
+                raise ValueError(f'Invalid css variable name')
+
+
+
 
     for root, dirs, files in os.walk("./images"):
         for file in files:
@@ -118,24 +109,39 @@ def main():
             if len(fileInfo) == 2 and ((fileInfo[1].lower()) in validExtensions):
                 print("Creating CSS files for " + file)
                 imageImportedSuccessfully = True
-                varName = ""
-                varName = varName.join(e for e in fileInfo[0].lower().title() if (e.isalnum() or e.isspace()))
-                varName = " ".join(varName.split())
+                imageVarName = ""
+                imageVarName = imageVarName.join(e for e in fileInfo[0].lower().title() if (e.isalnum() or e.isspace()))
+                imageVarName = " ".join(imageVarName.split())
+                
                 for cssType, cssTemplate in cssFileTypes.items():
                     if  imageImportedSuccessfully:
-                       imageImportedSuccessfully = writeCSSType(cssType, os.path.join(root, file), varName)
+                       imageImportedSuccessfully = writeCSSType(cssType, os.path.join(root, file), imageVarName)
                 
                 if imageImportedSuccessfully:
                     # Update Theme.json
                     print("Success")
-                    themeJson["patches"]["Home Screen Image"]["values"][varName] = { f"{cssDir}/{b64Dir}/{varName}.css": ["SP"], f"{cssDir}/{homeScreenCssDir}/{varName}.css": ["SP"], f"{homeScreenMainPath}": ["SP"]}
-                    themeJson["patches"]["Lock Screen Image"]["values"][varName] = { f"{cssDir}/{b64Dir}/{varName}.css": ["SP"], f"{cssDir}/{lockScreenCssDir}/{varName}.css": ["SP"], f"{lockScreenMainPath}": ["SP"]}
-                    themeJson["patches"]["Library Background Image"]["values"][varName] = { f"{cssDir}/{b64Dir}/{varName}.css": ["SP"], f"{cssDir}/{libraryCssDir}/{varName}.css": ["SP"], f"{libraryMainPath}": ["SP"]}
-                    themeJson["patches"]["Keyboard Background Image"]["values"][varName] = { f"{cssDir}/{b64Dir}/{varName}.css": ["SP"], f"{cssDir}/{keyboardCssDir}/{varName}.css": ["SP"], f"{keyboardMainPath}": ["SP"]}
+                    for k,v in themeJson["patches"].items():
+                        if v["type"] == "dropdown-image":
+                            print(f"Adding {imageVarName} for var {v['var']}")
+                            if "var" in v.keys() and re.match(r"[A-Za-z0-9_-]+", v["var"]):
+                                themeJson["patches"][k]["values"][imageVarName] = { f"{cssDir}/{b64Dir}/{imageVarName}.css": ["SP"], f'{cssDir}/{v["var"]}/{imageVarName}.css': ["SP"]}
+                            else:
+                                # No Var replace Normal dropdown Patch
+                                v["type"] = "dropdown"
+
+                    
                 else:
                     print("Failed")
     
     #print(json.dumps(themeJson, indent=4))
+
+    # Sanitize Extended Types
+    for k,v in themeJson["patches"].items():
+        if "var" in v.keys():
+            del v["var"]
+        if "type" in v.keys() and v["type"] == "dropdown-image":
+            v["type"] = "dropdown"
+
 
     f = open("theme.json" , "w")
     f.write(json.dumps(themeJson, indent=4))
